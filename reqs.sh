e908a408a9a7a8bdab1c87ef3c6989477bebf365
Antonio Murdaca <runcom@redhat.com>
2016-09-25 11:05:53 +0200

    add nfs
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- reqs.sh
+++ reqs.sh
@@ -7,4 +7,8 @@ systemctl unmask NetworkManager
 systemctl enable NetworkManager
 systemctl start NetworkManager
 git clone https://github.com/openshift/openshift-ansible $HOME/openshift-ansible
+systemctl enable nfs-server
+systemctl start nfs-server
+echo "/srv/nfs 127.0.0.1(rw,sync,no_root_squash)" >> /etc/exports
+systemctl restart nfs-server
 reboot
