40d294b95aee69042a4452d10b9e1e3dc58f9cc5
Antonio Murdaca <runcom@redhat.com>
2016-09-25 12:07:06 +0200

    more nfs
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- reqs.sh
+++ reqs.sh
@@ -7,8 +7,10 @@ systemctl unmask NetworkManager
 systemctl enable NetworkManager
 systemctl start NetworkManager
 git clone https://github.com/openshift/openshift-ansible $HOME/openshift-ansible
+mkdir -p /srv/nfs
+chown nfsnobody:nfsnobody /srv/nfs
+chmond 777 /srv/nfs
+echo "/srv/nfs *(rw,sync,all_squash)" >> /etc/exports
 systemctl enable nfs-server
 systemctl start nfs-server
-echo "/srv/nfs 127.0.0.1(rw,sync,no_root_squash)" >> /etc/exports
-systemctl restart nfs-server
 reboot
